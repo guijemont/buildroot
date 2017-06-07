9c82db0a77d6cba974da4f35220bdab4eff23953
Guillaume Emont <guijemont@igalia.com>
2017-06-07 11:04:24 -0500

    Compile jsc with debug symbols

--- package/jsconly/jsconly.mk
+++ package/jsconly/jsconly.mk
@@ -10,17 +10,26 @@ JSCONLY_INSTALL_STAGING = YES
 JSCONLY_DEPENDENCIES = host-bison host-cmake host-flex host-gperf host-ruby icu pcre
 
 JSCONLY_BUILD_JSC_ARGS = --jsc-only
-JSCONLY_BUILD_JSC_ARGS += \
-	--cmakeargs=-DCMAKE_TOOLCHAIN_FILE=${BR2_HOST_DIR}/usr/share/buildroot/toolchainfile.cmake
+JSCONLY_CMAKE_ARGS = -DCMAKE_TOOLCHAIN_FILE=${BR2_HOST_DIR}/usr/share/buildroot/toolchainfile.cmake
+
 
 ifeq ($(BR2_PACKAGE_JSC_DEBUG),y)
 JSCONLY_BUILD_JSC_ARGS += --debug
 JSCONLY_BUILD_DIR_NAME = Debug
+JSCONLY_CMAKE_ARGS += \
+	-DCMAKE_C_FLAGS_DEBUG='-g -O0' \
+	-DCMAKE_CXX_FLAGS_DEBUG='-g -O0'
 else
 JSCONLY_BUILD_JSC_ARGS += --release
 JSCONLY_BUILD_DIR_NAME = Release
+JSCONLY_CMAKE_ARGS += \
+	-DCMAKE_C_FLAGS_RELEASE='-O2 -g -DNDEBUG' \
+	-DCMAKE_CXX_FLAGS_RELEASE='-O2 -g -DNDEBUG'
 endif
 
+
+JSCONLY_BUILD_JSC_ARGS += \
+			  --cmakeargs="$(JSCONLY_CMAKE_ARGS)"
 define JSCONLY_BUILD_CMDS
 	(pushd $(@D) && \
 	PATH="${BR2_HOST_DIR}/ccache:${BR2_HOST_DIR}/usr/bin:${PATH}" ./Tools/Scripts/build-jsc ${JSCONLY_BUILD_JSC_ARGS} && \
